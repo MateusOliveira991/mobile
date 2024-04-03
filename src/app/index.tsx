7dc1768aef3a5afd3aabc95c98a21e883cb8f169
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-04-03 08:12:51 -0300

    C2: Estrutura estilização React Native

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,28 @@
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello, World!</Text>
+    <View style ={styles.container} >
+      <Text style={styles.title}>Hello, React Native!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: "red",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+
+    },
+    title: {
+
+        fontSize: 24,
+        color: "white",
+    }
+
+
+
+
+})
